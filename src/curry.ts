8c4b5671624a1ee92a23283678da19a235168e3d
Nick Fisher <manyyearsago@mail.ru>
2023-08-10 01:58:05 +0200

    curry finish

--- src/curry.ts
+++ src/curry.ts
@@ -1,3 +1,23 @@
-const curry = (fn: () => any, ...args: any) => {
-    
-}
+const curry = (fn: (...args: any) => any) => {
+  return function curried(...args: any) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    } else {
+      return function (...args2: any) {
+        return curried.apply(this, args.concat(args2));
+      };
+    }
+  };
+};
+
+const join = (a: any, b: any, c: any) => {
+  return `${a}_${b}_${c}`;
+};
+
+const curriedJoin = curry(join);
+
+console.log(curriedJoin(1, 2, 3)); // '1_2_3'
+
+console.log(curriedJoin(1)(2, 3)); // '1_2_3'
+
+console.log(curriedJoin(1, 2)(3)); // '1_2_3'
